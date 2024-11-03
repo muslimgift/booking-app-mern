25c8471c60eb94d6b8fd0f9625177daa83aa294b
Muhammad Muslim <muslimgift12345@gmail.com>
2024-11-03 16:10:22 +0500

    setting up the project

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.get("/api/tets", async (req:Request,res:Response)=>{
+    res.json({message:"helo from express endpoints!"})
+});
+
+app.listen(7000,()=>{
+    console.log("server is runing on 7000")
+})
